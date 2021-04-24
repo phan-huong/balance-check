af2f6cb60345645f8bac79e50a6e543af97da0aa
martinchristians <ms@imsimity.de>
2021-04-24 13:54:39 +0200

    Set up the project with Express.js (main.js, layout, static pages, css, homeController, errorController)

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,1 +1,13 @@
 "use strict";
+
+exports.homePage = (req, res) => {
+  res.render("index");
+}
+
+exports.showSignUp = (req, res) => {
+  res.render("contact");
+}
+
+exports.postedSignUpForm = (req, res) => {
+  res.render("thanks");
+}
--- main.js
+++ main.js
@@ -1,1 +1,40 @@
 "use strict";
+
+const port = 3000,
+      express = require("express"),
+      app = express(),
+      layouts = require("express-ejs-layouts"),
+      homeController = require("./controllers/homeController"),
+      errorController = require("./controllers/errorController");
+
+app.set("port", process.env.PORT || 3000);
+
+// set the application to use ejs
+app.set("view engine", "ejs");
+
+// set the application to use the layout module
+app.use(layouts);
+
+// serving of static files
+app.use(express.static("public"));
+
+// Routes
+app.get("/", homeController.homePage);
+app.get("/contact", homeController.showSignUp);
+app.post("/contact", homeController.postedSignUpForm);
+
+// Tell Express.js app to use body-parser for processing URL- encoded and JSON parameters
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
+
+// Add error handlers as middleware functions
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+
+app.listen(app.get("port"), () => {
+  console.log(`Server running at http://localhost:${app.get("port")}`);
+});
