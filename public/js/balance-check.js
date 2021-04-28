093d51b9d85b209a0567e2d74c019d190a8f480c
martinchristians <ms@imsimity.de>
2021-04-29 00:02:57 +0200

    create subtract function; display selected spending category + if condition when user choose custom
    subtract function is to subtract my balance with my spending

--- public/js/balance-check.js
+++ public/js/balance-check.js
@@ -5,19 +5,36 @@ function setVisibility(id, visibility) {
 }
 
 function myBalance(input, output) {
-  var m = document.getElementById(input).value;
+  var val = document.getElementById(input).value;
   var number = new Intl.NumberFormat("de-De", { //"en-GB" "en-GB"
     style: "currency",
     currency: "EUR" //"GBP" "USD"
   })
-  document.getElementById(output).innerHTML = "Your balance is: " + number.format(m);
+  document.getElementById(output).innerHTML = "Your balance is: " + number.format(val);
 }
 
 function mySpend(input, output) {
-  var m = document.getElementById(input).value;
-  var number = new Intl.NumberFormat("de-De", { //"en-GB" "en-GB"
+  var val = document.getElementById(input).value;
+  var obj = document.getElementById('selectCategory');
+  var number = new Intl.NumberFormat("de-De", {
     style: "currency",
-    currency: "EUR" //"GBP" "USD"
+    currency: "EUR"
   })
-  document.getElementById(output).innerHTML = "Spending: " + number.format(m);
+  if (obj.options[obj.selectedIndex].text != "Custom") {
+    document.getElementById(output).innerHTML = "Spending on " + obj.options[obj.selectedIndex].text + ": " + number.format(val);
+  } else {
+    // when select custom, create new field, where user can input text
+    document.getElementById(output).innerHTML = "";
+  }
+}
+
+function subtract(inputBalance, inputSpending, output) {
+  var balance = document.getElementById(inputBalance).value;
+  var spend = document.getElementById(inputSpending).value;
+  var number = new Intl.NumberFormat("de-De", {
+    style: "currency",
+    currency: "EUR"
+  })
+  // Display subtraction
+  document.getElementById(output).innerHTML = "Your end balance: " + number.format(balance-spend);
 }
