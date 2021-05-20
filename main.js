ac1f8bc666360ed1094d6b736903e02b493cba35
Phan Hương <phanngochuong@hotmail.com>
2021-05-20 10:19:59 +0200

    Connect MongoDB Cluster

--- main.js
+++ main.js
@@ -3,16 +3,25 @@
 const app = require('./app'),
       mongodbURI = process.env.MONGODB_URI || ((process.env.NODE_ENV === 'test') ? 'mongodb://localhost:27017/balance_check_test' : 'mongodb://localhost:27017/balance_check'),
       mongoose = require('mongoose'),
-      db = mongoose.connection;
+      // db = mongoose.connection;
+      db = require('./config/keys').mongoURI;
 
 // to support promise chains
 mongoose.Promise = global.Promise;
 
 // configuring mongoose with Node.js to set up database connection
-mongoose.connect(
-  mongodbURI,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+// mongoose.connect(
+//   mongodbURI,
+//   { useNewUrlParser: true, useUnifiedTopology: true }
+// );
+
+// configuring mongoose to connect with database
+mongoose.connect(db, {
+  useNewUrlParser: true, 
+  useCreateIndex: true, 
+  useUnifiedTopology: true, 
+  useFindAndModify: false
+});
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
@@ -21,6 +30,8 @@ db.once("open", () => {
   console.log("Successfully connected to MongoDB using Mongoose!");
 });
 
-app.listen(app.get("port"), () => {
-  console.log(`Server running at http://localhost:${app.get("port")}`);
-});
+// app.listen(app.get("port"), () => {
+//   console.log(`Server running at http://localhost:${app.get("port")}`);
+// });
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
