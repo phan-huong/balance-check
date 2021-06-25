5ff8a063ddaf17abb3a0137b9904e78ba334cc31
NiklasLengert <niklas.lengert@gmail.com>
2021-06-25 12:42:55 +0200

    balance-check

--- README.md
+++
@@ -1,8 +0,0 @@
-# Balance Check
-Project Web Technology SoSe21
-
-# Team (Group 6)
-- John-Peter Mason Warkulat
-- Martin Christian Solihin
-- Ngoc Huong Phan
-- Niklas Lengert
--- routes/homeRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const router = require("express").Router(),
-      homeController = require("../controllers/homeController");
-
-router.get("/", homeController.homePage);
-router.post("/myAccount", homeController.myAccount);
-
-module.exports = router;
