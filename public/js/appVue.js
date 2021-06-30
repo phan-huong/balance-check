e949b869173b727b248e888ffaf07507a1b3ddb9
martinchristians <ms@imsimity.de>
2021-06-30 14:38:04 +0200

    Change input category into select category

--- public/js/appVue.js
+++ public/js/appVue.js
@@ -2,7 +2,14 @@ var app = new Vue({
   el: '#app',
   data: {
     greeting: 'Welcome To Balance Check!',
-    image: '/images/goodVibesOnly.jpg'
+    image: '/images/goodVibesOnly.jpg',
+    options: [
+      { value: 'Groceries' },
+      { value: 'Barber' },
+      { value: 'Pet' },
+      { value: 'Other' }
+    ],
+    selected: ''
   },
   methods: {
     
