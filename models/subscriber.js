73e667057006f5db0c51d9ca41f1d58940318580
Phan Hương <64172294+phan-huong@users.noreply.github.com>
2021-07-15 16:53:34 +0200

    Revert "Pull main"

---
+++ models/subscriber.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const mongoose = require("mongoose"),
+      //define schema properties
+      subscriberSchema = mongoose.Schema({
+         name: String,
+         email: String,
+         zipCode: Number
+      });
+
+//export model so that other modules can access it
+module.exports = mongoose.model("Subscriber", subscriberSchema);
